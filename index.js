d592542ae2ec384e6d5997609ae0d30de89778de
sappster <marsapp@gmail.com>
2018-01-20 17:53:13 -0600

    [bug fix] typo in console log

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log('Example app listening at http://${host}:${port}')
+  console.log(`Example app listening at http://${host}:${port}`)
 });
